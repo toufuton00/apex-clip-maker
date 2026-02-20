e18df0590cc4bfb1630108d3151fdf4712983c39
toufuton00 <toufuton00@gmail.com>
2026-02-20 11:42:07 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',
+}
 
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
